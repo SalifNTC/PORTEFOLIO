e21eda4991f0c09720493489984e751ae6515c98
SalifNTC <Salifhenrique17@gmail.com>
2022-08-11 19:22:35 +0000

    correction

--- JavaScript/SendEmail.js
+++ JavaScript/SendEmail.js
@@ -1,38 +1,18 @@
 var senderName = document.getElementById("#senderName");
 var sender = document.getElementById("#sender");
 var content = document.getElementById("#content");
-function sendMail() {
 
-    //getting values from input fields
-  
-    sender = Form.sender.value;
-  
-
-    receiver= "salifhenrique17@gmail.com";
-  
-    content=Form.content.value;
-  
-   
-    //Sending email
-  
-    Email.send({
-  
-    Host: "smtp.gmail.com",
-  
-    Username: sender,
-  
-    To: receiver,
-  
-    From: sender,
-  
-    Subject: "Check Email Sending",
-  
-    Body: content,
-  
-    }).then(function (message) {
-  
-    alert("Email sent successfully")
-  
-    });
-  
-  }
+function sendEmail() { 
+	Email.send({ 
+		Host: "smtp.gmail.com", 
+		Username: "sender", 
+		Password: "Enter your password", 
+		To: 'salifhenrique17@gmail.com', 
+		From: sender, 
+		Subject: "Sending Email using javascript", 
+		Body: content, 
+	}) 
+		.then(function (message) { 
+		alert("email sent successfully") 
+		}); 
+	} 
